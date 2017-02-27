a3c73c8c599722712106594202e9dc8a1465b52a
Darren Vong <d.vongy@gmail.com>
2017-02-27 21:04:47 +0000

    Updated functions so now it can pull in posts for elections contents randomly

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ function my_enqueue_stuff() {
   wp_enqueue_script( 'jquery', get_template_directory_uri() . '/jquery.js');
   wp_enqueue_script( 'masonry', 'https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js');
   wp_enqueue_script( 'unslider', get_template_directory_uri() . '/unslider/unslider.js');
-  
+
   if ( is_page( 11189 ) ) {
 
     wp_enqueue_script( 'jQueryUI', 'http://code.jquery.com/ui/1.11.4/jquery-ui.js');
@@ -103,11 +103,12 @@ function first_post_image() {
 
 function twenty_posts_on_homepage( $query ) {
     if ( $query->is_home() && $query->is_main_query() ) {
-        $outlets = array( 'tv', 'press', 'sport' );
-        $posts_to_display = get_equal_num_posts_from_each_outlet( $outlets );
-        
+        $outlets = array( 'tv', 'press', 'sport', 'elections-2017'); // add 'varsity-2017' to this soon
+        $num_posts_to_display = 20;
+        $posts_to_display = get_equal_num_posts_from_each_outlet( $outlets, $num_posts_to_display );
+
         $query->set( 'post__in', $posts_to_display );
-        $query->set( 'posts_per_page', 20 );
+        $query->set( 'posts_per_page', $num_posts_to_display );
     }
 }
 add_action( 'pre_get_posts', 'twenty_posts_on_homepage' );
@@ -119,14 +120,18 @@ add_action( 'pre_get_posts', 'twenty_posts_on_homepage' );
  * evenly distributed
  * @return array  of ids of posts to be displayed on front page
  */
-function get_equal_num_posts_from_each_outlet( $outlets ) {
+function get_equal_num_posts_from_each_outlet( $outlets, $num_posts_to_display ) {
   $posts_to_display = array();
   foreach ( $outlets as $outlet ) {
-    $outlet_query = new WP_Query( array(
+    $query_args = array(
       'category_name' => $outlet,
-      'posts_per_page' => ($outlet === "tv")? 6 : 5,
+      'posts_per_page' => intval( $num_posts_to_display / count( $outlets ) ),
       'fields' => 'ids'
-    ));
+    );
+    // Randomise posts from Elections candidates
+    if ( $outlet === 'elections-2017' ) { $query_args['orderby'] = 'rand'; }
+
+    $outlet_query = new WP_Query( $query_args );
     $posts_to_display = array_merge($posts_to_display, $outlet_query->posts);
   }
   return $posts_to_display;
